14c554210b18710dff49375fa0e442699e0d02fe
JungSeoro <jsr@jungseoro.com>
2022-03-25 02:22:01 +0900

    Init repo

---
+++ create_project.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PROJECT_NAME=${1:-app}
+export $(cat .env | xargs)
+echo -e "\033[1;94mCreate \033[1;0m\033[1;104m$PROJECT_NAME\033[1;0m\033[1;94m project on Node.js v$NODE_VERSION\033[1;0m"
+docker run --user $UID -v $(pwd)/../:/vol node:$NODE_VERSION sh -c "npx create-react-app ~/$PROJECT_NAME && mv ~/$PROJECT_NAME/* /vol"
