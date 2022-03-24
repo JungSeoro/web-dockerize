65c4d906d8c6664351a02a26684d6394245bc002
JungSeoro <jsr@jungseoro.com>
2022-03-25 02:24:37 +0900

    Add creact code

--- create_project.sh
+++ create_project.sh
@@ -2,4 +2,4 @@
 PROJECT_NAME=${1:-app}
 export $(cat .env | xargs)
 echo -e "\033[1;94mCreate \033[1;0m\033[1;104m$PROJECT_NAME\033[1;0m\033[1;94m project on Node.js v$NODE_VERSION\033[1;0m"
-docker run --user $UID -v $(pwd)/../:/vol node:$NODE_VERSION sh -c "npx create-react-app ~/$PROJECT_NAME && mv ~/$PROJECT_NAME/* /vol"
+docker run --rm --user $UID -v $(pwd)/../:/vol node:$NODE_VERSION sh -c "npx create-react-app ~/$PROJECT_NAME && mv ~/$PROJECT_NAME/* /vol"
