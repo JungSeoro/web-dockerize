f334bb97ea9057c0e3db03bdc9bc010568379abc
JungSeoro <jsr@jungseoro.com>
2025-02-11 20:33:20 +0900

    Update ts init sciprt

--- create-project-react.sh
+++ create-project-react.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# Usage: ./create-project-react.sh <Project Name>
-# Example: ./create-project-react.sh myapp
+# ./create-project-react.sh <Project Name> [--ts]
+# Example: ./create-project-react.sh myapp --ts
 
 PROJECT_NAME=${1:-app}
-export $(cat .env | xargs)
-echo -e "\033[1;94mCreate \033[1;0m\033[1;104m$PROJECT_NAME\033[1;0m\033[1;94m project on Node.js v$NODE_VERSION\033[1;0m"
-docker run --rm --user $UID -v $(pwd)/../:/vol node:$NODE_VERSION sh -c "npx create-react-app ~/$PROJECT_NAME && npm install --prefix  ~/$PROJECT_NAME web-vitals && mv ~/$PROJECT_NAME/* /vol"
+TEMPLATE=""
+IS_TS=false
 
-INIT_REACT_DIR="./init_react"
+if [ "$2" == "--ts" ]; then
+    TEMPLATE="--template typescript"
+    IS_TS=true
+fi
+
+export $(cat .env | xargs)
+echo -e "\033[1;94mCreating \033[1;0m\033[1;104m$PROJECT_NAME\033[1;0m\033[1;94m project on Node.js v$NODE_VERSION\033[1;0m"
+docker run --rm --user $UID -v $(pwd)/../:/vol node:$NODE_VERSION sh -c "\
+  npx create-react-app ~/$PROJECT_NAME $TEMPLATE && \
+  npm install --prefix  ~/$PROJECT_NAME web-vitals && \
+  mv ~/$PROJECT_NAME/* /vol"
+
+if [ "$IS_TS" = true ]; then
+    INIT_REACT_DIR="./init_react_ts"
+else
+    INIT_REACT_DIR="./init_react"
+fi
+
 DEST_DIR="../"
 if [ -d "$INIT_REACT_DIR" ]; then
     echo "Copying initial React setup files..."
